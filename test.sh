2a1b4f3ff07779b0dd8461f8533a0f81eeb8087e
LordZeuss <87180353+LordZeuss@users.noreply.github.com>
2022-03-31 03:32:55 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -58,9 +58,10 @@ echo "Would you like to install Docker Compose (Required)? (y/n)"
 read yesorno
 
 if [ "$yesorno" = y ]; then
-	sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-	sudo chmod +x /usr/local/bin/docker-compose
-	sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+	yes | sudo apt install docker-compose
+	sudo groupadd docker
+	sudo gpasswd -a ${USER} docker
+	su - $USER
 	echo "Installation Successful."
 elif [ "$yesorno" = n ]; then
 	echo "Skipping..."
