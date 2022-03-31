a9b5fd37f13e4234f39f4cae7f66750f45625380
LordZeuss <87180353+LordZeuss@users.noreply.github.com>
2022-03-31 03:31:05 -0500

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#This is a script to help install Mullvad VPN for the Raspberry pi
+
+######################################################################
+
+#Functions List
+
+noanswer () { echo "Skipping..." ; }
+update () { yes | sudo apt-get update && sudo apt-get upgrade; }
+
+######################################################################
+
+#
+#System Update
+#
+
+echo "Would you like to update the system (Recommended)? (y/n)"
+
+read yesorno
+
+if [ "$yesorno" = y ]; then
+	update
+	echo "Update Successful."
+elif [ "$yesorno" = n ]; then
+	echo "Skipping..."
+else
+	echo "Not a valid answer. Exiting..."
+fi
+
+
+
+#
+#Docker Script Install
+#
+
+echo "Would you like to install Docker (Required)? (y/n)"
+
+read yesorno
+
+if [ "$yesorno" = y ]; then
+	yes | sudo apt-get install docker.io
+	sudo systemctl start docker
+	sudo systemctl enable docker
+	echo "Installation Successful."
+elif [ "$yesorno" = n ]; then
+	echo "Skipping..."
+else
+	echo "Not a valid answer. Exiting..."
+fi
+
+
+
+#Docker Compose Install
+
+echo "Would you like to install Docker Compose (Required)? (y/n)"
+
+read yesorno
+
+if [ "$yesorno" = y ]; then
+	sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+	sudo chmod +x /usr/local/bin/docker-compose
+	sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+	echo "Installation Successful."
+elif [ "$yesorno" = n ]; then
+	echo "Skipping..."
+else
+	echo "Not a valid answer. Exiting..."
+fi
+
